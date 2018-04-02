f6c2a412d0726c8097baada93b541ff9a2eb2823
tosh <toshism@gmail.com>
2018-04-01 20:07:56 -0500

    save data in dummy server to sqlite db

--- dummy_server.go
+++ dummy_server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"html"
+	"io"
 	"log"
 	"net/http"
 	"time"
@@ -36,15 +38,63 @@ func structRequest(w http.ResponseWriter, r *http.Request) {
 
 func printRequest(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(r.Body)
-	s := buf.String()
-	fmt.Println(s)
+	// buf := new(bytes.Buffer)
+	// buf.ReadFrom(r.Body)
+	// s := buf.String()
+	// fmt.Println(s)
+
+	// var dat map[string]interface{}
+	dec := json.NewDecoder(r.Body)
+	for {
+		var m PostBody
+		if err := dec.Decode(&m); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s: %s\n", m.Window.Title, m.AppKey)
+		app_id := get_or_create_app(m.Window.AppName)
+
+		insert_activity(m.Window, app_id)
+	}
 }
 
+func insert_activity(w *Window, app_id int64) {
+	_, err := db.Exec("INSERT INTO activity (name, started_time, end_time, duration, app_id) VALUES ($1, $2, $3, $4, $5)",
+		w.Title, w.Start_time, w.End_time, w.Duration, app_id)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func get_or_create_app(appName string) (id int64) {
+	row := db.QueryRow("SELECT id FROM apps WHERE name = $1", appName)
+	err := row.Scan(&id)
+	if err != nil && err != sql.ErrNoRows {
+		log.Fatal(err)
+	} else if err == sql.ErrNoRows {
+		result, err := db.Exec("INSERT INTO apps (name) VALUES ($1)", appName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		id, _ = result.LastInsertId()
+	}
+	return id
+}
+
+var db *sql.DB
+
 func main() {
+	var err error
+	db, err = sql.Open("sqlite3", "./x.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// defer db.Close()
+
 	fmt.Println("waiting...")
 	http.HandleFunc("/v1/event/add", printRequest)
 
 	log.Fatal(http.ListenAndServe(":5000", nil))
+
 }
---
+++ x.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS apps (
+       id integer PRIMARY KEY,
+       name text NOT NULL,
+       created_at datetime
+);
+
+
+CREATE TABLE IF NOT EXISTS activity (
+       id integer PRIMARY KEY,
+       name text NOT NULL,
+       started_time datetime NOT NULL,
+       end_time text NOT NULL,
+       duration INTEGER NOT NULL,
+       app_id INTEGER NOT NULL,
+       FOREIGN KEY (app_id) REFERENCES apps (id)
+);

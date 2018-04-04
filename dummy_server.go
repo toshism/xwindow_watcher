f4e4f04eb7b040a21d73b011afe903bf19944b79
tosh <toshism@gmail.com>
2018-04-03 23:02:14 -0500

    playing around with some charting on dummy server

--- dummy_server.go
+++ dummy_server.go
@@ -6,11 +6,34 @@ import (
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"html"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
+
+type Activity struct {
+	Id              int
+	Name            string
+	Started_time    time.Time
+	End_time        time.Time
+	Duration        time.Duration
+	DurationSeconds int64
+	App_id          int
+	AppName         string
+}
+
+type App struct {
+	Id         int
+	Name       string
+	Created_at time.Time
+}
+
+type ActivityList struct {
+	Activities []*Activity
+}
 
 type PostBody struct {
 	Window *Window
@@ -38,12 +61,6 @@ func structRequest(w http.ResponseWriter, r *http.Request) {
 
 func printRequest(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	// buf := new(bytes.Buffer)
-	// buf.ReadFrom(r.Body)
-	// s := buf.String()
-	// fmt.Println(s)
-
-	// var dat map[string]interface{}
 	dec := json.NewDecoder(r.Body)
 	for {
 		var m PostBody
@@ -57,6 +74,33 @@ func printRequest(w http.ResponseWriter, r *http.Request) {
 
 		insert_activity(m.Window, app_id)
 	}
+}
+
+type Page struct{}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("templates/index.html")
+	// p := Page{}
+	p := get_last_n_events(20)
+	t.Execute(w, p)
+}
+
+func get_last_n_events(n int) (activity_list *ActivityList) {
+	// rows, err := db.Query("SELECT * FROM activity JOIN apps ON apps.id = activity.app_id LIMIT ?", n)
+	rows, err := db.Query("SELECT activity.id, activity.name, activity.started_time, activity.end_time, activity.duration, activity.app_id, apps.name FROM activity JOIN apps ON apps.id = activity.app_id WHERE duration IS NOT NULL ORDER BY activity.started_time DESC LIMIT ?", n)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	activity_list = &ActivityList{}
+	for rows.Next() {
+		var activity = Activity{}
+		rows.Scan(&activity.Id, &activity.Name, &activity.Started_time, &activity.End_time, &activity.Duration, &activity.App_id, &activity.AppName)
+		fmt.Println(activity)
+		activity.DurationSeconds, _ = strconv.ParseInt(strconv.FormatFloat(activity.Duration.Seconds(), 'f', 0, 64), 10, 64)
+		activity_list.Activities = append(activity_list.Activities, &activity)
+	}
+	return activity_list
 }
 
 func insert_activity(w *Window, app_id int64) {
@@ -94,6 +138,7 @@ func main() {
 
 	fmt.Println("waiting...")
 	http.HandleFunc("/v1/event/add", printRequest)
+	http.HandleFunc("/", index)
 
 	log.Fatal(http.ListenAndServe(":5000", nil))
 
--- x.sql
+++ x.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS activity (
        id integer PRIMARY KEY,
        name text NOT NULL,
        started_time datetime NOT NULL,
-       end_time text NOT NULL,
+       end_time datetime NOT NULL,
        duration INTEGER NOT NULL,
        app_id INTEGER NOT NULL,
        FOREIGN KEY (app_id) REFERENCES apps (id)
